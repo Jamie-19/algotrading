e9e125b12cb8fab7ab9ed27b13ff90c8b8e3afaa
Jamie-19 <mdl21cs057@mec.ac.in>
2024-02-29 22:55:23 +0530

    convert to word

---
+++ wordconvert.py
@@ -0,0 +1,15 @@
+#Function to get the data in Docx format
+
+from docx import Document
+def generate_word_document(data):
+    doc = Document()
+    table = doc.add_table(rows=1, cols=len(data.columns))
+    for i, column in enumerate(data.columns):
+        table.cell(0, i).text = column
+
+    for _, row in data.iterrows():
+        row_cells = table.add_row().cells
+        for i, value in enumerate(row):
+            row_cells[i].text = str(value)
+
+    doc.save('Output/historical_data.docx')
