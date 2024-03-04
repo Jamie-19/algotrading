2c4e827462132bd4aae12ac6eb947cc15417d89e
Jamie-19 <mdl21cs057@mec.ac.in>
2024-03-05 00:33:45 +0530

    plot dcl dcm dcu

--- donchian.py
+++ donchian.py
@@ -88,7 +88,15 @@ aapl.tail()
 
 #PLotting the donchian channels
 
-
+plt.plot(aapl[-300:].close,label='Close Price',color='blue')
+plt.plot(aapl[-300:].dcl,color='black',linestyle='--',alpha=0.3)
+plt.plot(aapl[-300:].dcm,color='orange',label='DCM')
+plt.plot(aapl[-300:].dcu,color='black',linestyle='--',alpha=0.3,label='DCL,DCU')
+plt.legend()
+plt.title('AAPL stock price with Donchian Channels')
+plt.xlabel('Date')
+plt.ylabel('Close')
+plt.show()
 
 #Extracting the data in both word and text format
 to_word(aapl,'output/historical_data.docx') # if want to get more data please consider changing the code so as to convert to a text file else the code will be slow to run
