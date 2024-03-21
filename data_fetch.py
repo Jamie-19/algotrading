8f3c3c0b754fa15f7444a19e9169cd60dfec1118
Jamie-19 <mdl21cs057@mec.ac.in>
2024-03-21 16:32:27 +0530

    result csv

--- data_fetch.py
+++ data_fetch.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 def get_data(data):
     data.to_csv('output/historical_data.csv', index=True)
+
+def result_data(data):
+    data.to_csv('output/result/strategy_results.csv', index=True)
