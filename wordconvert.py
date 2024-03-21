c6cf756b8170a072d795fa136fbafd4bb557e244
Jamie-19 <mdl21cs057@mec.ac.in>
2024-03-21 16:32:51 +0530

    result word

--- wordconvert.py
+++ wordconvert.py
@@ -14,3 +14,18 @@ def to_word(data, filename):
             row_cells[i].text = str(value)
 
     doc.save(filename)
+
+#Function to convert the result to a word document
+def result_word(data,filename):
+    doc = Document()
+
+    table = doc.add_table(rows=1, cols=len(data.columns))
+    for i, column in enumerate(data.columns):
+        table.cell(0, i).text = column
+
+    for _, row in data.iterrows():
+        row_cells = table.add_row().cells
+        for i, value in enumerate(row):
+            row_cells[i].text = str(value)
+
+    doc.save(filename)
