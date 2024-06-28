65c588528def3a29fd65a2aadafd2a764cce4be6
Jamie-19 <mdl21cs057@mec.ac.in>
2024-06-29 00:14:40 +0530

    removed csv and added pdf file format

--- data_fetch.py
+++ data_fetch.py
@@ -1,9 +1,10 @@
 #Function to read the data
 
 import pandas as pd
+from pdf_file import df_to_pdf
 
 def get_data(data):
-    data.to_csv('output/historical_data.csv', index=True)
+    df_to_pdf(data, 'output/historical_data.pdf')
 
-def result_data(filename,data):
-    data.to_csv(filename, index=True)
+def result_data(filename, data):
+    df_to_pdf(data, filename)
